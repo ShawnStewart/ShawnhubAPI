a9e4ed7d1507f4495c23605426e8a5a5dca8202d
Shawn Stewart <shawn.stewart@quarkworks.co>
2019-07-11 18:18:48 -0500

    Removed test structure routes. All endpoints are now going to `/api/v1/`. Moved 404 and error handling routes from the API level to the app level.

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const morgan = require("morgan");
 const passport = require("passport");
 
 const { NODE_ENV } = process.env;
+const { InternalServerError, InvalidEndpointError } = require("./utils/errors");
 const { adminJwtStrategy, userJwtStrategy } = require("./utils/passport");
-const testStructure = require("./api/v1/routes");
-const apiV1Routes = require("./routes/api/v1");
+const apiV1Routes = require("./api/v1/routes");
 
 // Middlewares
 if (NODE_ENV === "dev") {
@@ -22,7 +22,33 @@ passport.use("admin-jwt", adminJwtStrategy);
 passport.use("user-jwt", userJwtStrategy);
 
 // Route requests
-app.use("/test-structure", testStructure);
 app.use("/api/v1", apiV1Routes);
+
+// Unknown endpoint
+app.use((req, res, next) => {
+    const err = new InvalidEndpointError(req.originalUrl);
+
+    return next(err);
+});
+
+// Error handling
+app.use((error, req, res, next) => {
+    const errDate = new Date()
+        .toISOString()
+        .replace(/T/, " ")
+        .replace(/\..+/, "");
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+    if (!error || !error.message.length) {
+        error = new InternalServerError();
+    }
+
+    if (NODE_ENV === "dev") {
+        console.log(`\n[${errDate}]\n${error.stack}`);
+    }
+
+    res.status(error.status || 500);
+    return res.json(error);
+});
 
 module.exports = app;
