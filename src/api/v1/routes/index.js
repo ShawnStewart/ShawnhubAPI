bbca90409a62a7d82e51a13a51988cd9ef58889a
Shawn Stewart <shawn.stewart@quarkworks.co>
2019-07-14 10:19:52 -0500

    Removed test route

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -2,11 +2,7 @@ const router = require("express").Router();
 const gameRoutes = require("./game");
 const userRoutes = require("./user");
 
-const testRoute = require("./routetest");
-
 router.use("/games", gameRoutes);
 router.use("/users", userRoutes);
-
-router.use("/test", testRoute);
 
 module.exports = router;
--- src/api/v1/routes/routetest.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-
-router.get("/", (req, res) => res.json({ newStructure: true, success: true }));
-
-module.exports = router;
