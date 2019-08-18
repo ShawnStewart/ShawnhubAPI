6042749a42a5d0b19f3d401bd2529f9938a1a0f8
Shawn Stewart <shawn.stewart@quarkworks.co>
2019-08-17 20:40:42 -0500

    Added socket initialize event to set variables. Added socket leave room event and updated join room event to check if the user is already in a room.

--- src/socket.js
+++ src/socket.js
@@ -2,19 +2,43 @@ const socket = require("socket.io");
 
 module.exports = socketSetup = (server) => {
     const io = socket.listen(server);
+
     io.on("connection", (socket) => {
         socket.on("test", (data) => {
             socket.emit("test", data);
         });
 
-        socket.on("joinRoom", (roomId, username) => {
-            console.log(`${username} is wanting to join ${roomId}`);
-            socket.username = username;
-            socket.join(roomId);
+        socket.on("initializeUser", (username) => {
+            console.log("[SOCKET] initializeUser", username);
+            socket.vars = {};
+            socket.vars.username = username;
+        });
+
+        socket.on("joinRoom", (roomId) => {
+            if (socket.vars.roomId) {
+                console.log(`already in ${socket.vars.roomId}, leaving`);
+                socket.leave(socket.vars.roomId);
+            }
+
+            socket.vars.roomId = roomId;
+            socket.join(socket.vars.roomId);
+            console.log(
+                `[SOCKET] ${socket.vars.username} joined ${socket.vars.roomId}`,
+            );
             io.to(roomId).emit(
                 "updateChat",
-                `${socket.username} join room ${roomId}`,
+                `${socket.vars.username} join room ${socket.vars.roomId}`,
             );
+        });
+
+        socket.on("leaveRoom", () => {
+            console.log(
+                `[SOCKET] ${socket.vars.username} is leaving ${
+                    socket.vars.rooms
+                }`,
+            );
+            socket.leave(socket.vars.roomId);
+            socket.vars.roomId = null;
         });
     });
 };
